87fa0acd0a2b58011b6fd304223dd608a369cebd
jubix-pixel <jefenedo4@github.com>
2022-08-08 22:40:13 -0700

    main.h

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Printf project
+for my team
---
+++ printf.c/convert_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+unsigned int convert_di(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_b(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_u(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_o(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+
+/**
+ *
